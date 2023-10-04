598e940985fc9e114cdb14d3676194f8f65a7cb3
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-04 23:02:55 +0330

    Make Drawer Items

--- lib/home.dart
+++ lib/home.dart
@@ -19,7 +19,91 @@ class HomeScreen extends StatelessWidget {
                 end: Alignment.topCenter,
               ),
             ),
-          )
+          ),
+          SafeArea(
+              child: Container(
+            width: 200,
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                DrawerHeader(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        child: Image.asset('images/1.jpg'),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        'Jovi Daniel',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView(
+                          children: const [
+                            ListTile(
+                              leading: Icon(
+                                Icons.home,
+                                color: Colors.white,
+                              ),
+                              title: Text(
+                                'Home',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.person,
+                                color: Colors.white,
+                              ),
+                              title: Text(
+                                'Profile',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.settings,
+                                color: Colors.white,
+                              ),
+                              title: Text(
+                                'Settings',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.logout,
+                                color: Colors.white,
+                              ),
+                              title: Text(
+                                'Log out',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ))
         ],
       ),
     );
